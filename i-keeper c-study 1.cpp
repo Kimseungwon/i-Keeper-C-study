f06615bd3d084488e780985609d7e31588a36975
Kimseungwon <163998143+Kimseungwon@users.noreply.github.com>
2024-03-22 16:44:00 +0900

    baekjoon 1000

---
+++ i-keeper c-study 1.cpp
@@ -0,0 +1,13 @@
+﻿// i-keeper c-study 1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+
+#include <stdio.h>
+
+int main()
+{
+    int A; int B;
+
+    scanf_s("%d%d", &A, &B);
+    printf("%d", A + B);
+
+    return 0;
+}
